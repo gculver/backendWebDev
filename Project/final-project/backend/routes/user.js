91413498cfce41f65a71146889d3883df8ef3c35
Grant Culver <grantaculver@gmail.com>
2019-11-24 13:06:42 -0600

    lesson 104

--- Project/final-project/backend/routes/user.js
+++ Project/final-project/backend/routes/user.js
@@ -1,11 +1,47 @@
 const express = require("express");
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const User = require("../models/user");
 
 const router = express.Router();
 
+router.post("/login", (req, res, next) => {
+  let fetchedUser;
+  User.findOne({ email: req.body.email })
+    .then(user => {
 
-router.post("/signup", (req, res, next) => {
+      if (!user) {
+        console.log(user);
+        return res.status(401).json({
+          message: "Auth Failed"
+        });
+      }
+      fetchedUser = user;
+      return bcrypt.compare(req.body.password, user.password);
+    })
+    .then(result => {
+      if ( !result ) {
+        return res.status(401).json({
+          message: "Auth Failed"
+        });
+      }
+      const token = jwt.sign({email: fetchedUser.email, userId: fetchedUser._id}, 'secret_this_should_be_longer', {expiresIn: '1h'});
+      res.status(200).json({
+
+        token: token
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      return res.status(401).json({
+        message: "Auth Failed"
+      });
+    });
+});
+
+router.post("/signup", (req, res) => {
+
   const user = new User({
     name: req.body.name,
     email: req.body.email,
@@ -24,22 +60,6 @@ router.post("/signup", (req, res, next) => {
         })
     })
   });
-  // bcrypt.hash(req.body.password, 10)
-  //   .then(hash => {
-
-  //     user.save()
-  //       .then(result => {
-  //         res.status(201).json({
-  //           message: "User created!",
-  //           result: result
-  //         });
-  //       })
-  //       .catch( err => {
-  //         res.status(500).json({
-  //           error:err
-  //         });
-  //       });
-  // });
 });
 
 
--- Project/final-project/src/app/views/users/login/login.component.ts
+++ Project/final-project/src/app/views/users/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from '../auth.service';
 
 @Component({
   styleUrls: ['./login.component.css'],
@@ -7,7 +8,13 @@ import { NgForm } from '@angular/forms';
 })
 export class LoginComponent {
 
+  constructor(public authService: AuthService) {}
+
   onLogin(form: NgForm) {
-    console.log(form.value);
+    if (form.invalid) {
+      return;
+    }
+    this.authService.login(form.value.email, form.value.password);
+
   }
 }
